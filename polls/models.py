238848a99fcbe140cf70e6f273f9e2d1e589f566
jean.angelis <jean.angelis@gmail.com>
2016-05-29 12:19:57 -0300

    Unit test created for question pub date

--- polls/models.py
+++ polls/models.py
@@ -14,7 +14,9 @@ class Question(models.Model):
         return self.question_text
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        recently = timezone.now() - datetime.timedelta(days=1)
+        return now >= self.pub_date >= recently
 
 
 class Choice(models.Model):
